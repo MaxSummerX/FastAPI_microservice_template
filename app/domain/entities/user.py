90e35be478623f8a866339b471687bb7690a0445
MaxSummerX <marspowerpops@gmail.com>
2026-05-28 11:33:24 +0300

    добавить метод для обноления данных сущности User

--- app/domain/entities/user.py
+++ app/domain/entities/user.py
@@ -8,3 +8,9 @@ class User:
     firstname: str
     lastname: str
     email: str
+
+    def update_profile(self, firstname: str | None = None, lastname: str | None = None) -> None:
+        if firstname:
+            self.firstname = firstname
+        if lastname:
+            self.lastname = lastname
