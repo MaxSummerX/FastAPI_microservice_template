13972a4cbbf31190de39b0d7e3226486b1f2c09f
MaxSummerX <marspowerpops@gmail.com>
2026-05-28 14:24:29 +0300

    подключить роутер пользователей к приложению

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 
+from app.presentation.routers.users import router
+
 
 app = FastAPI(
     title="User-service",
     version="0.1.0",
 )
+
+
+app.include_router(router, prefix="/api/V1")
 
 
 @app.get("/health_check", tags=["Health Check"])
