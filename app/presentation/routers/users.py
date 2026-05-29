66127b7dbfe4fff1b028b6398fa15e525faeaa41
MaxSummerX <marspowerpops@gmail.com>
2026-05-29 16:48:40 +0300

    добавить авторизацию в роутер пользователей и docstrings

--- app/presentation/routers/users.py
+++ app/presentation/routers/users.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from app.application.exception import UserNotFoundException
 from app.application.services.user_service import UserService
 from app.domain.entities.user import User
-from app.presentation.dependencies import get_user_service
+from app.presentation.dependencies import get_current_user, get_user_service
 from app.presentation.schemas.user import UserProfileUpdate, UserResponse
 
 
@@ -13,7 +13,14 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("/{user_id}", status_code=status.HTTP_200_OK)
-async def get_user(user_id: UUID, service: UserService = Depends(get_user_service)) -> UserResponse:
+async def get_user(
+    user_id: UUID,
+    current_user: User = Depends(get_current_user),
+    service: UserService = Depends(get_user_service),
+) -> UserResponse:
+    """Получает информацию о пользователе по его ID."""
+    if current_user.id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
     try:
         user: User = await service.get_by_id(user_id)
         return UserResponse.model_validate(user)
@@ -23,8 +30,14 @@ async def get_user(user_id: UUID, service: UserService = Depends(get_user_servic
 
 @router.patch("/{user_id}", status_code=status.HTTP_200_OK)
 async def update_user(
-    user_id: UUID, user_data: UserProfileUpdate, service: UserService = Depends(get_user_service)
+    user_id: UUID,
+    user_data: UserProfileUpdate,
+    current_user: User = Depends(get_current_user),
+    service: UserService = Depends(get_user_service),
 ) -> UserResponse:
+    """Обновляет информацию о пользователе по его ID."""
+    if current_user.id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
     try:
         user: User = await service.update_profile(
             user_id=user_id, firstname=user_data.firstname, lastname=user_data.lastname
