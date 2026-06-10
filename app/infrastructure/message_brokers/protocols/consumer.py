1c0d990f9c135c8234a8c4a8596e7b05d3c52dc2
MaxSummerX <marspowerpops@gmail.com>
2026-06-10 16:50:13 +0300

    добавить абстрактный класс IEventConsumer

---
+++ app/infrastructure/message_brokers/protocols/consumer.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from collections.abc import AsyncGenerator
+
+from app.domain.events.base import BaseEvent
+
+
+class IEventConsumer(ABC):
+    @abstractmethod
+    async def start(self) -> None:
+        """Установить соединение с брокером и начать чтение."""
+        pass
+
+    @abstractmethod
+    async def stop(self) -> None:
+        """Остановить чтение и закрыть соединение."""
+        pass
+
+    @abstractmethod
+    def subscribe(self, topic: str) -> AsyncGenerator[BaseEvent]:
+        """Подписаться на топик."""
+        pass
